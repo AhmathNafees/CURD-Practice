6b9edc564e115586aac7afa5869e08c94dc66f80
AhmathNafees <ahamednafees15@gmail.com>
2026-06-07 23:40:04 +0530

    upload img php implemented

--- image_upload.php
+++ image_upload.php
@@ -6,7 +6,18 @@
     }
 
     if(isset($_POST['submit'])){
-        
+        $file_name = $_FILES['image']['name'];
+        $tempName = $_FILES['image']['tmp_name'];
+        $folder = 'images/'.$file_name;
+
+        $query = mysqli_query($conn, "INSERT INTO image (file) VALUES ('$file_name')");
+
+        if(move_uploaded_file($tempName, $folder)){
+            echo "<h2>File Uploaded Successfully</h2>";
+        }
+        else{
+            echo "<h2>File Not Uploaded</h2>";
+        }
     }
 
 ?>
